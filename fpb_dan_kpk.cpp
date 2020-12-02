6f951a1a65bb07ebcbf7784f1d3d7268a45df652
Aimar Wibowo <aimarwibowo46@gmail.com>
2020-12-02 11:10:26 +0700

    Add fpb dan kpk

---
+++ fpb_dan_kpk.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int fpb(int x, int y) {
+	
+	int res;
+	for(int i = 1; i <= x && i <= y; i++) {
+		if(x % i == 0 && y % i == 0) {
+			res = i;
+		}
+	}
+	return res;
+}
+
+int fpb_rekursif(int x, int y) {
+	
+	if(x % y == 0) return y;
+	return fpb_rekursif(y, x % y);
+}
+
+int kpk(int x, int y) {
+	
+	return x * y / fpb(x, y); 
+}
+
+int main() {
+	
+	cout << fpb(48, 30) << endl;
+	cout << fpb_rekursif(48, 30) << endl;
+	cout << kpk(12, 15) << endl;
+	
+	return 0;
+}
